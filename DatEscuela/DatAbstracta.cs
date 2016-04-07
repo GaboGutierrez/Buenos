42f9cd7f3bc9fd082bea2adfa4b0ba3726a5f1db
leoabmen <ylia12@hotmail.com>
2016-04-06 21:10:21 -0500

    zp
    ok

--- DatEscuela/DatAbstracta.cs
+++ DatEscuela/DatAbstracta.cs
@@ -12,7 +12,7 @@ namespace Gabo.Escuela.Data
         public SqlConnection con;
         public DatAbstracta()
         {
-            con = new SqlConnection("Data Source = CASALAP; User Id=sa; Password=12345; Initial Catalog=Escuela;");
+            con = new SqlConnection("Data Source = LEONEL\\MSSQLSERVER2012; User Id=sa; Password=12345; Initial Catalog=Escuela;");
         
         }
     }
